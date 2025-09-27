1c8acf10815e330d356906557b5680e5909743da
youssefhelmy869 <youssefahmed1050@gmail.com>
2025-09-27 14:01:51 +0300

    safe threading now

--- screen.h
+++ screen.h
@@ -7,51 +7,58 @@ using namespace std;
 using namespace sf;
 
 
-
-// fix threading  and sfml contectiomn in thread ownership
-
 class screen
 {
 private:
-    RenderWindow window;
+    RenderWindow *window = nullptr;
     Image image;
     Texture texture;
     Sprite sprite;
     mutex mtx;            // to protect the data being shared
-    thread window_thread; // thread to run window
+    thread rendering_thread; // thread to run window
+    bool update_image = false;
+    bool runing = true;
 
     void create_window_loop()
     {
+        // make window
+        window = new RenderWindow(VideoMode(1000, 1000), "main window");
+        image.create(1000, 1000, Color::Black);
+        texture.loadFromImage(image);
+        sprite.setTexture(texture);
+
         // start loop
-        while (window.isOpen())
+        while (window->isOpen() && runing == true)
         {
+
             // close
             Event close_event;
 
-            while (window.pollEvent(close_event))
+            while (window->pollEvent(close_event))
             {
                 if (close_event.type == Event::Closed)
                 {
-                    window.close();
+                    window->close();
                 }
             }
-            window.clear();
+            if (update_image == true)
+            {
+                texture.update(image);
+                update_image = false;
+            }
+            window->clear();
             {
                 lock_guard<mutex> lock(mtx); // lock while drawing
-                window.draw(sprite);
+                window->draw(sprite);
             }
-            window.display();
+            window->display();
         }
     }
 
 public:
     screen()
 
-        : window(VideoMode(1000, 1000), "main window")
     {
-        image.create(1000, 1000, Color::Black);
-        texture.loadFromImage(image);
-        sprite.setTexture(texture);
     }
 
     void add_pixsel(int x_position, int y_postion, Color colour = Color::White)
@@ -59,23 +66,24 @@ public:
 
         lock_guard<mutex> lock(mtx);
         image.setPixel(x_position, y_postion, colour);
-
-        texture.update(image);
+        update_image = true;
     }
     void run()
     {
-        window_thread = thread(screen::create_window_loop, this);
-
+        rendering_thread = thread(screen::create_window_loop, this);
     }
     ~screen()
     {
-        if (window.isOpen())
+        runing = false;
+
+        if (window && window->isOpen())
         {
-            window.close();
+            window->close();
         }
-        if (window_thread.joinable())
+        if (window && rendering_thread.joinable())
         {
-            window_thread.join();
+            rendering_thread.join();
         }
+
     }
 };
